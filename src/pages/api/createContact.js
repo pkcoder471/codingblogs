03b4a0b25acc420e8dd6439f0d591581edb82def
pkcoder471 <pklid471@gmail.com>
2024-02-13 19:59:50 +0530

    some changes

--- src/pages/api/createContact.js
+++ src/pages/api/createContact.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 export default async function handler(req, res) {
 
     try {
-        const blogs = await fs.promises.readdir('public/ContactData');
-        const len = blogs.length;
+        const contacts = await fs.promises.readdir('public/ContactData');
+        const len = contacts.length;
         const newFile = await fs.promises.writeFile(`public/ContactData/${len+1}.json`,JSON.stringify(req.body));
 
         res.status(200).json(newFile);
